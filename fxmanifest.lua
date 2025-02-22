71a55836b62c05e66748e70af4eb23bda032466b
Alivemonstor [Jayden] <79489495+Alivemonstor@users.noreply.github.com>
2025-02-22 02:44:17 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,19 +5,12 @@ author 'Alivemonstor'
 description 'Cinematic Trader'
 version '1.0.0'
 
-client_scripts {
-    'client.lua',
-    '@PolyZone/client.lua',
-    '@PolyZone/BoxZone.lua',
-    '@PolyZone/CircleZone.lua',
+client_script 'client.lua'
+
+shared_scripts {
+    '@ox_lib/init.lua',
 }
-shared_script 'config.lua'
 server_script 'server.lua'
 
-ui_page 'html/index.html'
-
-files {
-    'html/*'
-}
 
 lua54 'yes'
